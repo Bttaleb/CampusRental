dd11740842f4448658c2a33ca8eb14769e67d2ce
Bttaleb <btalebg@gmail.com>
2026-02-13 17:39:08 -0500

    initial

---
+++ SoftwareEngTests/SoftwareEngTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoftwareEngTests.swift
+//  SoftwareEngTests
+//
+//  Created by Bassel Taleb on 2/6/26.
+//
+
+import Testing
+@testable import SoftwareEng
+
+struct SoftwareEngTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
